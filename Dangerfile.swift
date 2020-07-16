def3b81bb108dc3ccd8c52515c533b01baefe1d0
Florian Friedrich <ffried@me.com>
2020-07-16 08:43:28 +0200

    Update Dangerfile

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,16 +1,17 @@
 import Foundation
 import Danger
 
-fileprivate extension Danger.File {
-    var isDangerfile: Bool {
-        self == "Dangerfile.swift"
-    }
-}
-
 let danger = Danger()
-//let git = danger.git
+let git = danger.git
+let changedFiles = git.modifiedFiles + git.createdFiles
+
+if Bool.random(), let randomFile = changedFiles.randomElement() {
+   let lines = danger.utils.readFile(randomFile).split(separator: "\n")
+   if let randomLine = lines.indices.randomElement() {
+      markdown(message: "Random remark", file: randomFile, line: randomLine + 1)
+   }
+}
 
 if Bool.random() {
-   danger.warn("Random warning")
+   warn("Random warning")
 }
-
